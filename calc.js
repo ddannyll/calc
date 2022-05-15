bda7837b902ea31e660d4cf25ed12f1ac3e51193
ddannyll <daniel.nguyen.s173@gmail.com>
2022-05-15 11:01:55 +1000

    feat: implement basic calculations from buttons

--- calc.js
+++ calc.js
@@ -1,4 +1,10 @@
+const screen = document.querySelector('#calculator .screen')
 
+let calc = {
+    a: "",
+    b: null,
+    operator: null
+}
 
 function add(a, b) {
     return a + b
@@ -32,3 +38,59 @@ function operate(operator, a, b) {
     }
 }   
 
+function resetCalc() {
+    calc = {
+        a: "",
+        b: null,
+        operator: null
+    }
+}
+
+function appendNum(curr, num) {
+    (curr === null) ? curr = num : curr += num
+    return curr
+}
+
+function displayStr(str) {
+    screen.innerText = str
+}
+
+const buttons = document.querySelectorAll(".button-grid button")
+buttons.forEach((button) => {
+    switch (button.className) {
+        case 'operator':
+            button.onclick = () => {
+                calc.operator = button.value
+                calc.b = null
+            }
+            break
+        case 'operand':
+            button.onclick = () => {
+                if (calc.operator === null) {
+                    calc.a = appendNum(calc.a, button.value);
+                    displayStr(calc.a)
+                } else {
+                    calc.b = appendNum(calc.b, button.value)
+                    displayStr(calc.b)
+                }
+            }
+            break
+        case 'equals':
+            button.onclick = () => {
+                result = operate(calc.operator, +calc.a, +calc.b)
+                calc.a = result
+                displayStr(calc.a)
+            }
+            break
+        case 'clear':
+            button.onclick = () => {
+                resetCalc()
+                displayStr('0')
+            }
+            break
+        default:
+            return null
+    }
+})
+
+displayStr('0')
